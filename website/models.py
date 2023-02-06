24f4bd8ee53860f73721f6483d2840068fa2d243
veeexy <marasek515@gmail.com>
2023-02-06 16:23:41 +0100

    comments

--- website/models.py
+++ website/models.py
@@ -9,43 +9,43 @@ class Note(db.Model):
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-class Reservation(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    ticket_id = db.Column(db.Integer, db.ForeignKey('ticket.id'))
-    guide_id = db.Column(db.Integer, db.ForeignKey('guide.id'))
-    safari_id = db.Column(db.Integer, db.ForeignKey('safari.id'))
-    tickets = db.relationship('Ticket')
-    users = db.relationship('User')
-    guides = db.relationship('Guide')
-    safari = db.relationship('Safari')
+# class Reservation(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+#     ticket_id = db.Column(db.Integer, db.ForeignKey('ticket.id'))
+#     guide_id = db.Column(db.Integer, db.ForeignKey('guide.id'))
+#     safari_id = db.Column(db.Integer, db.ForeignKey('safari.id'))
+#     tickets = db.relationship('Ticket')
+#     users = db.relationship('User')
+#     guides = db.relationship('Guide')
+#     safari = db.relationship('Safari')
     
 
-class Safari(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    date = db.Column()
-    type = db.Column()
-    seats = db.Column()
+# class Safari(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     date = db.Column()
+#     type = db.Column()
+#     seats = db.Column()
 
-class Guide(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    guide_name = db.Column(db.String(150))
-    # surname = db.Column(db.String(150))
+# class Guide(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     guide_name = db.Column(db.String(150))
+#     # surname = db.Column(db.String(150))
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
-    user_name = db.Column(db.String(150))
-    phone_number = db.Column(db.Integer)
+    # user_name = db.Column(db.String(150))
+    # phone_number = db.Column(db.Integer)
     # tickets = db.relationship('Ticket')
 
-class Ticket(db.Model):
-    id = db.Column(db.Integer, primary_key =True)
-    price = db.Column()
-    amount = db.Column()
-    type = db.Column()
-    ticket_date = db.Column()
-    # user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+# class Ticket(db.Model):
+#     id = db.Column(db.Integer, primary_key =True)
+#     price = db.Column()
+#     amount = db.Column()
+#     type = db.Column()
+#     ticket_date = db.Column()
+#     # user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
